41327d71643dec171ee020ce8bb2f803c8c2a6d9
kellergoech <kellergoech@gmx.de>
2020-06-22 12:06:24 +0200

    firstcommit

--- app/src/main/java/de/zw/locity/MainActivity.kt
+++ app/src/main/java/de/zw/locity/MainActivity.kt
@@ -1,11 +1,26 @@
 package de.zw.locity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.parse.Parse
+import com.parse.ParseObject
+
+import de.zw.locity.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Parse.initialize(Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id)) // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        )
+
+
+
+        val myFirstClass = ParseObject("MyFirstClass")
+        myFirstClass.put("name", "I'm able to save objects!")
+        myFirstClass.saveInBackground()
     }
 }
